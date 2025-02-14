83c8860d13604d0bf53589d7a44a509247734041
Calvin Wong <calvinwongchin@gmail.com>
2025-02-14 03:02:09 +0000

    Create new file for institution comparison page

---
+++ pages/institution_comparison.py
@@ -0,0 +1,26 @@
+import dash
+from dash import callback, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from utils.dashboard_components import create_card, format_value
+
+# read in datasets
+results_df = pd.read_csv('data/results_cleaned.csv')
+income_df = pd.read_csv('data/income_cleaned.csv')
+incomeiK_df = pd.read_csv('data/incomeiK_cleaned.csv')
+phd_df = pd.read_csv('data/phd_awarded_cleaned.csv')
+
+dash.register_page(
+    module= __name__,
+    external_stylesheets = [dbc.themes.BOOTSTRAP, 'assets/style.css'],
+    path = '/institution_comparison'
+)
+
+layout = dbc.Container(
+    [
+        "Hello World!!"
+    ]
+)
